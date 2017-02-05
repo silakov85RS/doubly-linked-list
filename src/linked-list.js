886966d97c20fba9e737f927284a70a2f486c8d8
silakov85RS <silakov.rolling.scopes@gmail.com>
2017-02-05 20:56:23 +0200

    DLL/feature: make some tests

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,23 +1,88 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
 
-    head() {}
+        if(this.length == 0) {
+            this._tail = this._head = node;
+        }
+        else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        this.length++;
+        return this;
+    }
 
-    tail() {}
+    head() {
+        return this._head.data;
+    }
 
-    at(index) {}
+    tail() {
+        return this._tail.data;
+    }
 
-    insertAt(index, data) {}
+    _at(index) {
+        var x = this._head;
+        for(var i = 0; i != index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
 
-    isEmpty() {}
+    at(index) {
+        return this._at(index).data;
+    }
 
-    clear() {}
+    insertAt(index, data) {
+        if(index == this.length) {
+            this.append(data);
+        }
+        this._at(index).data = data;
+    }
 
-    deleteAt(index) {}
+    isEmpty() {
+        if(this.length == 0) {
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+    clear() {
+        var count = this.length - 1;
+        for(var x = 0; x <= this.length; x++) {
+            var i = count - x;
+            while (i >= 0) {
+                var n = this._head.data;
+                n = null;
+                this.deleteAt(i);
+                i--;
+            }
+        }
+    }
+
+    deleteAt(index) {
+        var node = this._head;
+        var removeNode = this._at(index);
+        if(this.length == 1) {
+            constructor();
+        }
+        else {
+            removeNode.prev.next = removeNode.next;
+        }
+        this.length--;
+        return this;
+    }
 
     reverse() {}
 
